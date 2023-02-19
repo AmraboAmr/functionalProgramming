b4fc9ef98206caa9de57293079809ac7f823df0c
amroaboamro <amer.abuamer444@gmail.com>
2023-02-19 19:38:51 +0200

    321

--- detailsScript.js
+++ detailsScript.js
@@ -1,1 +1,87 @@
+let details=document.getElementById("details");
 
+let countryId = window.location.search.split('id=')[1];
+
+function getDetails(data) {
+    let name,native,subRegion,tld,currencies,languages, population, region, capital, flag;
+
+
+
+
+
+console.log(data);
+        name = data.name.common;
+        population = data.population;
+        region = data.region;
+
+        capital = data.capital;
+        flag = data.flags.svg;
+
+
+
+
+    native=data.name.nativeName[Object.keys(data.name.nativeName)[0]].official;
+
+
+    subRegion=data.subregion;
+tld=data.tld[0];
+
+        details.innerHTML += `
+    <div id="flag" class="col-lg-5">
+    <img class="img-fluid  " src="${flag}"  alt="${name}"></div>
+            <div class="col-lg-6 py-4 h-75">
+                <div class="fs-2   fw-bold ">${name}</div>
+                <div class="fs-6 fw-semibold row ">
+
+                    <div class="col col-lg-6 mt-4">
+
+
+                        <div class="">Native Name: <span class="text-muted">${native}</span></div>
+                        <div class="">Population: <span class=" text-muted">${population}</span></div>
+                        <div class="">Region: <span class="text-muted">${region}</span></div>
+                        <div class="">Sub Region: <span class="text-muted">${subRegion}</span></div>
+                        <div class="">Capital: <span class="text-muted">${capital}</span></div>
+
+                    </div>
+                    <div class="mt-4 col-lg-6 ">
+
+
+                        <div class="">Top Level Domain: <span class="text-muted">${tld}</span></div>
+
+                        <div class="">Currencies: <span class="text-muted">Euro</span></div>
+                        <div class="">Languages: <span class="text-muted">German,Serbian,North Frisian</span></div>
+
+
+                    </div>
+                </div>
+                <div class="row justify-content-start g-3 mt-4" >
+                    <div  class="fw-semibold col-lg-auto mt-4 ">Border Countries: </div>
+                    <div class="col-auto ">
+                        <button class="btn shadow-sm bg-white px-4 me-1" type="button">France</button>
+                        <button class="btn shadow-sm bg-white px-4 me-1" type="button">Poland</button>
+                        <button class="btn shadow-sm bg-white px-4 me-1" type="button">Netherlands</button></div>
+
+
+                </div>
+
+            </div>
+`;
+
+
+
+}
+
+function getCountries(){
+    let url=`https://restcountries.com/v3.1/name/${countryId}`;
+    fetch(url)
+        .then ((response)=> response.json())
+        .then((data)=>{
+
+            getDetails(data[0]);
+
+        });
+
+
+
+}
+getCountries();
--- homeScript.js
+++ homeScript.js
@@ -1,15 +1,32 @@
-
+let searchBtn=document.getElementById("searchButton");
 let searchInput=document.getElementById("searchInput");
 let countries = document.getElementById("countries");
 let dropDownBtn=document.getElementById("dropDown");
+let i=0;
+//let e=20;
+const NO_FILTER ="No Filter";
 let filter={
 
 };
-filter='All';
+filter=NO_FILTER;
 function filtering(value){
    filter= value;
     dropDownBtn.innerHTML=value;
-    search();
+    let cards=document.getElementsByClassName("country")
+    if(filter!=NO_FILTER){
+        let filterCountries=document.getElementsByClassName(filter);
+        for(let x of cards){
+            x.style.display="none";
+        }
+        for(let x of filterCountries){
+            x.style.display="block";
+        }
+    }
+    else{
+        for(let x of cards){
+            x.style.display="block";
+        }
+    }
 }
 function clear(){
     countries.innerHTML="";
@@ -17,8 +34,21 @@ function clear(){
 }
 function getData(data) {
     let name, population, region, capital, flag;
+let c,j;
+// if(filter==NO_FILTER)e=20;
+// else{
+//     e=data.length;
+// }
 
-    for (let c of data) {
+
+    //for( j=i ;j<i+e;j++){
+    for(let c of data){
+        // if(j>=c.length){
+        //
+        //     i=0;
+        //     return;
+        // }
+
 
         name = c.name.common;
         population = c.population;
@@ -27,9 +57,8 @@ function getData(data) {
         capital = c.capital;
         flag = c.flags.svg;
 
-
-        countries.innerHTML += `  <a onclick="getElementId(this.id)"  class="     col-xl-3 col-md-4   " href="details.html">
-        <div class="card border-0 shadow-sm  h-100 " >
+        countries.innerHTML += `  <a   class=" country col-xl-3 col-md-4 ${region}  " href="details.html?id=${name}">
+        <div class="card border-0 shadow-sm  h-100  " >
             <img class="card-img-top  " src="${flag}" alt="${name}" >
             <div class="card-body fw-semibold">
                 <div class="card-title fs-5 fw-bold">${name}</div>
@@ -39,11 +68,13 @@ function getData(data) {
             </div>
         </div>
     </a>`;
-break;
+
     }
+   // i=j;
+
 }
 
-function getAll(){
+function getCountries(){
     let url=`https://restcountries.com/v3.1/all`;
     fetch(url)
         .then ((response)=> response.json())
@@ -55,10 +86,11 @@ function getAll(){
         });
 }
 function search(){
+    //i=0;
    let searchValue=searchInput.value;
    if(searchValue==""){
        clear();
-       getAll();
+       getCountries();
        return;
    }
 
@@ -67,9 +99,17 @@ function search(){
         .then ((response)=> response.json())
         .then((data)=>{
             clear();
+            console.log(data);
           getData(data);
         });
 }
-
+// window.onscroll = function() {
+//     if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
+//
+//        getCountries();
+//
+//     }
+// }
 searchInput.addEventListener("keyup",search);
-getAll();
+searchBtn.addEventListener("click",search);
+getCountries();
